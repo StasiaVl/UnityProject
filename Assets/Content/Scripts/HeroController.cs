47922f0ebf44f93dcafa32dc19e6d7518bc51d28
StasiaVl <strange-stasia@ukr.net>
2018-05-21 20:50:20 +0300

    Test scene
    Rabbit now moves right-left and stands with proper animations.
    Camera and background sky follow the rabbit.
    Three small platforms (made prefab) on the scene with edge colliders

---
+++ Assets/Content/Scripts/HeroController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+    public float speed = 1;
+
+    private Rigidbody2D myBody = null;
+    private SpriteRenderer sr = null;
+    Animator anim;
+    int runHash;
+    int idleHash;
+
+    // Use this for initialization
+    void Start()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        runHash = Animator.StringToHash("Rabbit_Run");
+        idleHash = Animator.StringToHash("Rabbit_Idle");
+    }
+
+    void FixedUpdate()
+    {
+        float value = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(value) > 0)
+        {
+            anim.Play("Rabbit_Run");
+            Vector2 vel = myBody.velocity;
+            vel.x = value * speed;
+            myBody.velocity = vel;
+
+            if (value < 0)
+            {
+                sr.flipX = true;
+            }
+            else if (value > 0)
+            {
+                sr.flipX = false;
+            }
+        }
+        else anim.Play("Rabbit_Idle");
+    }
+}
---
+++ Assets/Content/Scripts/HeroFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour
+{
+
+    public HeroController rabit;
+
+    private Transform rabit_transform = null;
+    private Transform camera_transform = null;
+    private Vector3 rabit_position;
+    private Vector3 camera_position;
+
+    void Update()
+    {
+        //Отримуємо доступ до компонента Transform
+        //це Скорочення до GetComponent<Transform>
+        rabit_transform = rabit.transform;
+
+        //Отримуємо доступ до компонента Transform камери
+        camera_transform = this.transform;
+
+        //Отримуємо доступ до координат кролика
+        rabit_position = rabit_transform.position;
+        camera_position = camera_transform.position;
+
+        //Рухаємо камеру тільки по X,Y
+        camera_position.x = rabit_position.x;
+        camera_position.y = rabit_position.y;
+
+        //Встановлюємо координати камери
+        camera_transform.position = camera_position;
+    }
+}
